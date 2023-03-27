e74b29a232e204b4b8ccf1d65a7e721df22addaf
igorlebedev <lebedew.igor400@yandex.ru>
2023-03-27 16:06:57 +0700

    rework for i18next

--- src/app.js
+++ src/app.js
@@ -1,48 +1,55 @@
 import onChange from 'on-change';
-import validator from '../utils/validator.js';
+import i18n from 'i18next';
+import validator from './utils/validator.js';
 import { renderMessage, renderPage } from './view.js';
+import ru from './locales/ru.js';
+
+const initFormListener = (form, state, watchedState) => form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const inputValue = formData.get('url');
+  const urlObj = { url: inputValue };
+  validator(urlObj, state.feeds)
+    .then(({ url }) => {
+      watchedState.isValid = true;
+      watchedState.feeds.push(url);
+      watchedState.error = null;
+      const input = document.getElementById('url-input');
+      form.reset();
+      input.focus();
+    })
+    .catch((error) => {
+      watchedState.isValid = false;
+      watchedState.error = error.type;
+    });
+});
 
 export default () => {
-  const state = {
-    feeds: [],
-    error: null,
-  };
-
-  const watchedState = onChange(state, (path, value) => {
-    if (path === 'error') {
-      renderMessage(value);
-    }
-    if (path === 'feeds') {
-      renderMessage(state.error);
-      renderPage(value);
-    }
-  });
-
   const form = document.querySelector('.rss-form');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const inputValue = formData.get('url');
-    const urlObj = { url: inputValue };
-    validator(urlObj, state.feeds)
-      .then(({ url }) => {
-        watchedState.feeds.push(url);
-        watchedState.error = null;
-        const input = document.getElementById('url-input');
-        form.reset();
-        input.focus();
-      })
-      .catch((error) => {
-        switch (error.type) {
-          case 'url':
-            watchedState.error = 'Ссылка должна быть валидным URL';
-            break;
-          case 'notOneOf':
-            watchedState.error = 'RSS уже существует';
-            break;
-          default:
-            throw new Error(`unexpected error type: ${error.type}`);
+
+  const i18nInst = i18n.createInstance();
+  i18nInst.init({
+    lng: 'ru',
+    resources: {
+      ru,
+    },
+  })
+    .then(() => {
+      const state = {
+        isValid: null,
+        feeds: [],
+        error: null,
+      };
+
+      const watchedState = onChange(state, (path, value) => {
+        if (path === 'error') {
+          renderMessage(state.isValid, value, i18nInst);
+        }
+        if (path === 'feeds') {
+          renderPage(value);
         }
       });
-  });
+
+      initFormListener(form, state, watchedState);
+    });
 };
--- src/view.js
+++ src/view.js
@@ -1,16 +1,21 @@
-const renderMessage = (value) => {
+const renderMessage = (isValid, value, i18nInst) => {
   const messageContainer = document.querySelector('.feedback');
   const urlInput = document.getElementById('url-input');
-  if (value === null) {
-    urlInput.classList.remove('is-invalid');
-    messageContainer.textContent = 'RSS успешно загружен';
-    messageContainer.classList.add('text-success');
-    messageContainer.classList.remove('text-danger');
-  } else {
-    urlInput.classList.add('is-invalid');
-    messageContainer.textContent = value;
-    messageContainer.classList.add('text-danger');
-    messageContainer.classList.remove('text-success');
+  switch (isValid) {
+    case true:
+      urlInput.classList.remove('is-invalid');
+      messageContainer.textContent = i18nInst.t('messages.success');
+      messageContainer.classList.add('text-success');
+      messageContainer.classList.remove('text-danger');
+      break;
+    case false:
+      urlInput.classList.add('is-invalid');
+      messageContainer.textContent = i18nInst.t(`messages.errors.${value}`);
+      messageContainer.classList.add('text-danger');
+      messageContainer.classList.remove('text-success');
+      break;
+    default:
+      throw new Error(`Unexpected state: ${isValid}`);
   }
 };
 
