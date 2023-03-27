1e1b677d3e4f2245af23912f6fc3f3b4e79aecdf
igorlebedev <lebedew.igor400@yandex.ru>
2023-03-27 16:07:32 +0700

    add ru locale

---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    messages: {
+      errors: {
+        url: 'Ссылка должна быть валидным URL',
+        notOneOf: 'RSS уже существует',
+      },
+      success: 'RSS успешно загружен',
+    },
+  },
+};
