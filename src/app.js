f3749dcf0a0713d7511050b2b0f225422bc58a6a
igorlebedev <igorlebedev228@gmail.com>
2023-03-28 20:41:30 +0700

    main logic rework

--- src/app.js
+++ src/app.js
@@ -2,35 +2,42 @@ import i18n from 'i18next';
 import axios from 'axios';
 import xmlParser from './utils/xmlParser.js';
 import validator from './utils/validator.js';
+import rssStateBuilder from './utils/rssStateBuilder.js';
 import initWatchedState from './view.js';
 import ru from './locales/ru.js';
 
-const initFormListener = (form, state, watchedState) => form.addEventListener('submit', (e) => {
+const initFormListener = (form, watchedState) => form.addEventListener('submit', (e) => {
   e.preventDefault();
+  watchedState.process = 'processing';
   const formData = new FormData(e.target);
   const inputValue = formData.get('url');
-  validator(inputValue, state.feeds)
-    .then((correctUrl) => {
-      watchedState.isValid = true;
-      watchedState.feeds.push(correctUrl);
-      watchedState.error = null;
+  const activeUrls = watchedState.rss.feeds.map(({ url }) => url);
+  validator(inputValue, activeUrls)
+    .then(() => axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(inputValue)}`))
+    .then((response) => {
+      const parsed = xmlParser(response.data.contents);
+      rssStateBuilder(inputValue, parsed, watchedState);
       const input = document.getElementById('url-input');
       form.reset();
       input.focus();
-      return correctUrl;
-    })
-    .then((correctUrl) => axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(correctUrl)}`))
-    .then((response) => {
-      xmlParser(response.data.contents);
+      watchedState.error = null;
+      watchedState.process = 'success';
     })
     .catch((error) => {
-      watchedState.isValid = false;
       watchedState.error = error.message;
+      watchedState.process = 'error';
     });
 });
 
 export default () => {
-  const form = document.querySelector('.rss-form');
+  const state = {
+    process: null,
+    rss: {
+      feeds: [],
+      posts: [],
+    },
+    error: null,
+  };
 
   const i18nInst = i18n.createInstance();
   i18nInst.init({
@@ -40,14 +47,9 @@ export default () => {
     },
   })
     .then(() => {
-      const state = {
-        isValid: null,
-        feeds: [],
-        error: null,
-      };
-
       const watchedState = initWatchedState(state, i18nInst);
 
-      initFormListener(form, state, watchedState);
+      const form = document.querySelector('.rss-form');
+      initFormListener(form, watchedState);
     });
 };
