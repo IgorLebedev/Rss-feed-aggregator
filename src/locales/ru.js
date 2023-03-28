804ef2328bfa618bb9f31b4a464ef6edd0feaa5e
igorlebedev <igorlebedev228@gmail.com>
2023-03-28 20:42:13 +0700

    add some translations and another error

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,11 +1,17 @@
 export default {
   translation: {
+    posts: 'Посты',
+    feeds: 'Фиды',
+    buttons: {
+      view: 'Просмотр',
+    },
     messages: {
       errors: {
         'Incorrect Url': 'Ссылка должна быть валидным URL',
         'Already Exists': 'RSS уже существует',
         'Network Error': 'Ошибка сети',
         'Invalid RSS': 'Ресурс не содержит валидный RSS',
+        'Incorrect RSS': 'Неизвестная ошибка. Что-то пошло не так',
       },
       success: 'RSS успешно загружен',
     },
