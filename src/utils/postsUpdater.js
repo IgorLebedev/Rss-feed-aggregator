674fa9042847d2bdf60a7dce93aa27dc10ca3851
igorlebedev <igorlebedev228@gmail.com>
2023-03-30 13:31:35 +0700

    minor fix

--- src/utils/postsUpdater.js
+++ src/utils/postsUpdater.js
@@ -3,21 +3,26 @@ import getter from './getter.js';
 import xmlParser from './xmlParser.js';
 import rssStateBuilder from './rssStateBuilder.js';
 
-const updater = (currentUrl, watchedState, feedId) => setTimeout(() => getter(currentUrl)
+const updater = (currentUrl, watchedState, currentfeedId) => setTimeout(() => getter(currentUrl)
   .then((response) => {
     watchedState.process = 'updating';
     const parsedRss = xmlParser(response.data.contents);
     const oldPosts = watchedState.rss.posts;
+    const filteredById = oldPosts.filter(({ feedId }) => feedId === currentfeedId);
     const [, postsArr] = rssStateBuilder(currentUrl, parsedRss);
     const comparator = (arrayValue, otherValue) => arrayValue.title === otherValue.title;
-    const newPosts = _.differenceWith(postsArr, oldPosts, comparator);
-    const newPostsWithIds = newPosts.map((post) => ({ feedId, id: _.uniqueId(), ...post }));
+    const newPosts = _.differenceWith(postsArr, filteredById, comparator);
+    const newPostsWithIds = newPosts.map((post) => ({
+      feedId: currentfeedId,
+      id: _.uniqueId(),
+      ...post,
+    }));
     if (newPostsWithIds.length > 0) {
       oldPosts.unshift(...newPostsWithIds);
     }
     watchedState.process = 'success';
   })
-  .then(() => updater(currentUrl, watchedState))
+  .then(() => updater(currentUrl, watchedState, currentfeedId))
   .catch((e) => {
     throw new Error(e);
   }), 5000);
