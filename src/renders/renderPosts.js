7a70aff95b04c0b4f9a9e1cc37461b39a26d9175
igorlebedev <igorlebedev228@gmail.com>
2023-03-30 18:17:53 +0700

    add link clicked logic

--- src/renders/renderPosts.js
+++ src/renders/renderPosts.js
@@ -1,4 +1,8 @@
-export default (posts, i18nInst) => {
+import renderModal from './renderModal.js';
+
+const renderPosts = (watchedState, i18nInst) => {
+  const { posts } = watchedState.rssData;
+  const { uiState } = watchedState;
   const postsContainer = document.querySelector('.posts');
   postsContainer.innerHTML = '';
 
@@ -18,31 +22,46 @@ export default (posts, i18nInst) => {
   const ul = document.createElement('ul');
   ul.classList.add('list-group', 'border-0', 'rounded-0');
 
-  posts.forEach(({ id, title, link }) => {
+  posts.forEach((post) => {
     const li = document.createElement('li');
     li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
     const a = document.createElement('a');
-    a.href = link;
-    a.classList.add('fw-bold');
-    a.setAttribute('data-id', id);
+    a.href = post.link;
+    a.setAttribute('data-id', post.id);
     a.setAttribute('target', '_blank');
     a.setAttribute('rel', 'noopener');
     a.setAttribute('rel', 'noreferrer');
-    a.textContent = title;
+    a.textContent = post.title;
+    if (uiState[post.id] === 'read') {
+      a.classList.add('fw-normal', 'link-secondary');
+    } else { a.classList.add('fw-bold'); }
+
+    a.addEventListener('click', () => {
+      uiState[post.id] = 'read';
+      renderPosts(watchedState, i18nInst);
+    });
 
     const button = document.createElement('button');
     button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
     button.setAttribute('type', 'button');
-    button.setAttribute('data-id', id);
+    button.setAttribute('data-id', post.id);
     button.setAttribute('data-bs-toggle', 'modal');
     button.setAttribute('data-bs-target', '#modal');
     button.textContent = i18nInst.t('buttons.view');
 
+    button.addEventListener('click', () => {
+      uiState[post.id] = 'read';
+      renderPosts(watchedState, i18nInst);
+      renderModal(post, watchedState);
+    });
+
     li.append(a);
     li.append(button);
-    ul.append(li);
+    ul.prepend(li);
   });
 
   postsContainer.append(mainCard);
   postsContainer.append(ul);
 };
+
+export default renderPosts;
