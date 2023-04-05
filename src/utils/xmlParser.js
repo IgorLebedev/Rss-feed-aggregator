f7e23e4b2ad59f207550d83fb966e8862c22a362
igorlebedev <igorlebedev228@gmail.com>
2023-04-05 13:32:18 +0700

    moved to parsing

--- src/utils/xmlParser.js
+++
@@ -1,8 +0,0 @@
-export default (data) => {
-  const parser = new DOMParser();
-  const result = parser.parseFromString(data, 'application/xml');
-  if (result.querySelector('parsererror')) {
-    throw new Error('Invalid RSS');
-  }
-  return result;
-};
