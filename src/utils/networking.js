17a4a41b20ce62a1b472471891517c62ce77a729
igorlebedev <igorlebedev228@gmail.com>
2023-04-12 13:29:37 +0700

    rework logic to promise all

--- src/utils/networking.js
+++ src/utils/networking.js
@@ -1,28 +1,32 @@
 import _ from 'lodash';
 import axios from 'axios';
-import { xmlParser, rssStateBuilder } from './parsing.js';
+import { xmlParser, postsStateBuilder } from './parsing.js';
 
 export const getter = (url) => axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`);
 
-export const updater = (currentUrl, watchedState, currentfeedId) => getter(currentUrl)
-  .then((response) => {
-    watchedState.parsingProcess = 'updating';
-    const parsedRss = xmlParser(response.data.contents);
-    const oldPosts = watchedState.rssData.posts;
-    const filteredById = oldPosts.filter(({ feedId }) => feedId === currentfeedId);
-    const [, postsArr] = rssStateBuilder(currentUrl, parsedRss);
-    const comparator = (arrayValue, otherValue) => arrayValue.title === otherValue.title;
-    const newPosts = _.differenceWith(postsArr, filteredById, comparator);
-    const newPostsWithIds = newPosts.map((post) => ({
-      feedId: currentfeedId,
-      id: _.uniqueId(),
-      ...post,
-    }));
-    if (newPostsWithIds.length > 0) {
-      oldPosts.unshift(...newPostsWithIds);
-    }
-    watchedState.parsingProcess = 'updated';
-  })
-  .catch((e) => {
-    throw new Error(e);
-  });
+export const updater = (watchedState) => {
+  const { feeds } = watchedState.rssData;
+  const addGetter = feeds.map(({ url }) => getter(url));
+  return Promise.all(addGetter)
+    .then((responses) => {
+      responses.forEach((response) => {
+        watchedState.updatingProcess = 'updating';
+        const parsedRss = xmlParser(response.data.contents);
+        const oldPosts = watchedState.rssData.posts;
+        const postsArr = postsStateBuilder(parsedRss);
+        const comparator = (arrayValue, otherValue) => arrayValue.title === otherValue.title;
+        const newPosts = _.differenceWith(postsArr, oldPosts, comparator);
+        if (newPosts.length > 0) {
+          const newPostsWithIds = newPosts.map((post) => ({
+            id: _.uniqueId(),
+            ...post,
+          }));
+          oldPosts.unshift(...newPostsWithIds);
+        }
+      });
+      watchedState.updatingProcess = 'updated';
+    })
+    .catch((e) => {
+      throw new Error(e);
+    });
+};
